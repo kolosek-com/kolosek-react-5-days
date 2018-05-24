843bb26a07e3ad4e67e3f947159346c0f194a8c9
Kristina G <kristina@kolosek.com>
2018-05-24 10:01:43 +0200

    Day 2: JSX conditions, loops, styling and component communication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import LoopRendering from './components/LoopRendering/LoopRendering';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LoopRendering />
       </div>
     );
   }
