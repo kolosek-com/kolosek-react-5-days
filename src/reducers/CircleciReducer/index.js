93b5bfdc57d919ee3c1a0e2a00b2b632e93c13df
MiroslavKolosek <miroslav@kolosek.com>
2018-08-30 12:39:59 +0200

    Use ImmutableJS record for reducer initial state

--- src/reducers/CircleciReducer/index.js
+++ src/reducers/CircleciReducer/index.js
@@ -1,35 +1,26 @@
 import * as action_types from './constants';
+import { Record } from 'immutable';
 
-const initialState = {
+const InitialState = Record({
   buildList: [],
   selectedBuild: null,
-};
+});
+
+const initialState = new InitialState();
 
 const circleciReducer = (state = initialState, action) => {
   switch (action.type) {
     case action_types.GET_CIRCLECI_BUILDS_START:
-      return {
-        ...state
-      }
+      return state.set('buildList', [])
     case action_types.GET_CIRCLECI_BUILDS_SUCCESS:
-      return {
-        ...state,
-        buildList: action.data
-      }
+      return state.set('buildList', action.data)
     case action_types.SELECT_CIRCLECI_BUILD:
-      return {
-        ...state,
-        selectedBuild: action.data
-      }
+      return state.set('selectedBuild', action.data)
     case action_types.GET_CIRCLECI_BUILDS_ERROR:
-      return {
-        ...state
-      }
+      return state
     default:
-      break;
+      return state
   }
-
-  return state
 }
 
 export default circleciReducer
